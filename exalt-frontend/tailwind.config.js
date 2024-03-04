a0bdc2ad043dd4001f042db20178fe7cbb0691f2
Sammy-05 <m.sarim12966@gmail.com>
2024-03-04 14:34:24 +0500

    Setting up the Project

---
+++ exalt-frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {},
+    colors: {
+      navyblue: "#051C2C",
+      black: "#060200",
+      lightblue: "#00A9F4",
+      violet: "#2251FF",
+      white: "#ffffff",
+      grey: "#F0F0F0",
+    },
+  },
+  plugins: [],
+}
+
