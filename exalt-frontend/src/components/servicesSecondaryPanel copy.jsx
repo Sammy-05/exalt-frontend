998f26affd4d69aa85bb30edf1a12af68c5322ba
KabirK71 <kabirkateja71@gmail.com>
2024-03-08 01:40:46 +0500

    adjusted the height

--- exalt-frontend/src/components/servicesSecondaryPanel copy.jsx
+++ exalt-frontend/src/components/servicesSecondaryPanel copy.jsx
@@ -6,7 +6,7 @@ const ServicesSecondaryPanel = (props) => {
   return (
     <div className=" w-full text-white bg-navyblue flex text-left">
       <div className="w-full flex  items-center justify-center text-center">
-        <div className="flex flex-col gap-y-10 w-5/6 my-10 justify-center items-center">
+        <div className="flex flex-col gap-y-10 w-5/6 my-44 justify-center items-center">
           <div className="font-georgia text-mainheading">
             <h1 className='text-mainheading'>{heading}</h1>
           </div>
