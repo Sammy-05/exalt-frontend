8b8e8e5aef4d8a5577cc76047b0420527b193fb1
KabirK71 <kabirkateja71@gmail.com>
2024-03-14 03:24:49 +0500

    home page made responsive

--- exalt-frontend/src/components/insights.jsx
+++ exalt-frontend/src/components/insights.jsx
@@ -1,6 +1,7 @@
 import blogPhoto1 from "../assets/blogPhoto1.png";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import blogsData from '../assets/blogsData.json';
 
 const blogList = [
   {
@@ -19,36 +20,37 @@ const Insights = () => {
   //   const { heading, text, buttontext } = props;
 
   return (
-    <div className=" bg-white w-full px-20 py-10">
+    <div className=" bg-white w-full px-10 md:px-20 py-5 md:py-10">
       <div>
-        <h1 className="text-mainheading font-georgia font-normal ">
+        <h1 className="text-mobile-mainheading md:text-mainheading font-georgia font-normal ">
           Insights and Updates
         </h1>
-        <p className="font-roboto font-normal text-subheading mt-3 mb-10">
+        <p className="font-roboto font-normal text-mobile-subheading md:text-subheading mt-3 mb-10">
           Stay ahead of the curve with the latest trends and tips from our
           industry experts.
         </p>
       </div>
 
-      <div className="grid grid-cols-2 gap-x-20">
-        {blogList.map((blog) => (
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-x-20">
+        {blogsData.slice(0,2).map((blog) => (
           <div className="w-full flex justify-center items-center">
             <div
               key={blog["blog number"]}
-              className="flex flex-col text-white  bg-navyblue rounded-2xl"
+              className="flex flex-col text-white my-5 bg-navyblue rounded-2xl"
             >
               <div className="w-full">
                 <img
                   src={blogPhoto1}
                   alt="meeting"
-                  className="w-full h-[300px]"
+                  className="w-full h-[200px] md:h-[300px] rounded-2xl"
                 />
               </div>
-              <div className="px-10 py-5 flex flex-col gap-y-10">
-                <h1 className="font-georgia font-normal text-heading ">
-                  {blog.heading}
+              <div className="px-5 md:px-10 py-5 flex flex-col gap-y-10">
+                <h1 className="font-georgia font-normal text-mobile-heading  md:text-heading ">
+                  {blog.mainHeading}
                 </h1>
-                <p className="font-roboto font-normal text-p ">{blog.blog}</p>
+                <h2 className="font-roboto font-normal text-mobile-subheading md:text-subheading ">{blog.mainSubHeading}</h2>
+                <p className="font-roboto font-normal text-mobile-p md:text-p  h-48 ">{blog.displayText}</p>
               </div>
               <div className="w-full flex justify-end items-center pt-5 mb-3">
                 <div className="group">
@@ -71,7 +73,7 @@ const Insights = () => {
           <FontAwesomeIcon icon={faArrowRight} className=" ml-4" />
         </button>
       </div> */}
-      <div className="flex justify-center pt-20">
+      <div className="flex justify-center pt-10 md:pt-20">
           <button className="font-georgia font-bold hover:bg-violet bg-navyblue text-white px-5 rounded-xl py-2 flex border-2 border-white items-center">
             Start Your Journey With Us
             <FontAwesomeIcon
