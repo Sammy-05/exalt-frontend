5ccf14d1b9d782f46fd4b57f8f0b17e4bb58efb5
KabirK71 <kabirkateja71@gmail.com>
2024-04-09 21:55:30 +0500

    blog uploading works

--- exalt-frontend/src/pages/UploadBlog.jsx
+++ exalt-frontend/src/pages/UploadBlog.jsx
@@ -1,19 +1,22 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import QuillEditor from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import '../custom.css';
 
 const UploadBlog = () => {
-  const [mainHeading, setMainHeading] = useState("");
-  const [subheading, setSubheading] = useState("");
-  const [displayText, setDisplayText] = useState("");
-  const [mainContent, setMainContent] = useState("");
+  const [mainHeading, setMainHeading] = useState('');
+  const [subHeading, setSubHeading] = useState('');
+  const [displayText, setDisplayText] = useState('');
+  const [mainContent, setMainContent] = useState('');
   const [authorPic, setAuthorPic] = useState(null);
   const [displayPic, setDisplayPic] = useState(null);
-  const [authorName, setAuthorName] = useState("");
+  const [authorName, setAuthorName] = useState('');
   const [loading, setLoading] = useState(false);
   const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3001/blogs").then((response) => {
+    axios.get('http://localhost:3001/blogs').then((response) => {
       console.log(response.data);
       setBlogs(response.data);
     });
@@ -22,19 +25,26 @@ const UploadBlog = () => {
   const handleUpload = async () => {
     setLoading(true);
 
+    console.log('main heading', mainContent);
+    console.log('display', displayText);
+
     const requestData = {
       Main_Heading: mainHeading,
+      Sub_Heading: subHeading,
+      Display_Text: displayText,
+      Author_Name: authorName,
+      Author_Pic: authorPic,
       Main_Content: mainContent,
       Display_Pic: displayPic,
     };
 
     try {
-      await axios.post("http://localhost:3001/save_blog", requestData);
+      await axios.post('http://localhost:3001/save_blog', requestData);
       setLoading(false);
-      alert("Blog uploaded successfully");
+      alert('Blog uploaded successfully');
     } catch (error) {
       setLoading(false);
-      alert("Failed to upload blog");
+      alert('Failed to upload blog');
     }
   };
 
@@ -49,7 +59,7 @@ const UploadBlog = () => {
     };
 
     fileReader.onerror = (error) => {
-      console.error("Error: ", error);
+      console.error('Error: ', error);
     };
   }
   function convertToBase64AuthorPic(e) {
@@ -63,12 +73,12 @@ const UploadBlog = () => {
     };
 
     fileReader.onerror = (error) => {
-      console.error("Error: ", error);
+      console.error('Error: ', error);
     };
   }
 
   return (
-    <div className="flex flex-col h-screen items-center justify-center gap-y-10">
+    <div className="flex flex-col  items-center justify-center gap-y-10">
       <h1 className="text-mainheading text-violet font-georgia">Upload Blog</h1>
       <input
         className="border-2 border-blogtext p-2 rounded-lg w-1/4"
@@ -81,15 +91,22 @@ const UploadBlog = () => {
         className="border-2 border-blogtext p-2 rounded-lg w-1/4"
         type="text"
         placeholder="Sub Heading"
-        value={subheading}
-        onChange={(e) => setSubheading(e.target.value)}
+        value={subHeading}
+        onChange={(e) => setSubHeading(e.target.value)}
       />
-      <input
+      {/* <input
         className="border-2 border-blogtext p-2 rounded-lg w-1/4"
         type="text"
         placeholder="Display Text"
         value={displayText}
         onChange={(e) => setDisplayText(e.target.value)}
+      /> */}
+      <QuillEditor
+        className="h-48 w-1/4 p-2"
+        theme="snow"
+        value={displayText}
+        onChange={(value) => setDisplayText(value)}
+        // onChange={(e) => setMainContent(e.target.value)}
       />
       <input
         className="border-2 border-blogtext p-2 rounded-lg w-1/4"
@@ -98,14 +115,23 @@ const UploadBlog = () => {
         value={authorName}
         onChange={(e) => setAuthorName(e.target.value)}
       />
-      <textarea
+      {/* <textarea
         className="border-2 border-blogtext p-2 rounded-lg w-1/4"
         placeholder="Content"
         value={mainContent}
         onChange={(e) => setMainContent(e.target.value)}
-      ></textarea>
+      ></textarea> */}
+      <QuillEditor
+        className="h-48 w-1/4 p-2"
+        theme="snow"
+        value={mainContent}
+        onChange={(value) => setMainContent(value)}
+        // onChange={(e) => setMainContent(e.target.value)}
+      />
       <div>
-        <h1 className="font-georgia text-button pb-2">Upload Background Picture</h1>
+        <h1 className="font-georgia text-button pb-2">
+          Upload Background Picture
+        </h1>
         <input type="file" accept="image/*" onChange={convertToBase64} />
       </div>
       <div>
@@ -120,16 +146,23 @@ const UploadBlog = () => {
       {displayPic && <img width={100} height={100} src={displayPic} alt="" />}
       {authorPic && <img width={100} height={100} src={authorPic} alt="" />}
 
-      <button className="px-5 py-3 bg-navyblue border-2 border-white hover:bg-white hover:border-2 hover:border-navyblue text-white text-button hover:text-navyblue" onClick={handleUpload}>Upload Blog</button>
+      <button
+        className="px-5 py-3 bg-navyblue border-2 border-white hover:bg-white hover:border-2 hover:border-navyblue text-white text-button hover:text-navyblue"
+        onClick={handleUpload}
+      >
+        Upload Blog
+      </button>
 
-      {/* {blogs.map((blog) => (
+      {blogs.map((blog) => (
         <div>
-          <h1>{blog.Main_Heading}</h1>
-          <p>{blog.Main_Content}</p>
-          <img src={blog.Display_Pic} alt="" />
+          <div
+            className="blogPostContent"
+            dangerouslySetInnerHTML={{ __html: blog.Main_Content }}
+          />
+
+          
         </div>
-      ))
-        } */}
+      ))}
     </div>
   );
 };
